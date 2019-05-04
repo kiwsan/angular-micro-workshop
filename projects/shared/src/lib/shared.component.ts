e129729907810b074fb6f3fe0705c5368a1cc48c
kiwsan <kiwsanthia@gmail.com>
2019-05-04 16:54:45 +0700

    add shared projects

---
+++ projects/shared/src/lib/shared.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'shared-shared',
+  template: `
+    <p>
+      shared works!
+    </p>
+  `,
+  styles: []
+})
+export class SharedComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -7,11 +7,24 @@ import {AppComponent} from './app.component';
 
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
-import {DiscoverModule} from '../../projects/discover/src/lib/discover.module';
-import {MoviesModule} from '../../projects/movies/src/lib/movies.module';
-import {ReviewsModule} from '../../projects/reviews/src/lib/reviews.module';
-import {SearchModule} from '../../projects/search/src/lib/search.module';
-import {TrendingModule} from '../../projects/trending/src/lib/trending.module';
+import {DiscoverModule} from '../../projects/features-modules/discover/src/lib/discover.module';
+import {MoviesModule} from '../../projects/features-modules/movies/src/lib/movies.module';
+import {ReviewsModule} from '../../projects/features-modules/reviews/src/lib/reviews.module';
+import {SearchModule} from '../../projects/features-modules/search/src/lib/search.module';
+import {TrendingModule} from '../../projects/features-modules/trending/src/lib/trending.module';
+import {ConfigService} from '../../projects/shared/src/lib/app-config.service';
+
+/**
+ * Calling functions or calling new is not supported in metadata when using AoT.
+ * The work-around is to introduce an exported function.
+ *
+ * The reason for this limitation is that the AoT compiler needs to generate the code that calls the factory
+ * and there is no way to import a lambda from a module, you can only import an exported symbol.
+ */
+
+export function configServiceFactory(config: ConfigService) {
+  return () => config.load();
+}
 
 @NgModule({
   declarations: [
